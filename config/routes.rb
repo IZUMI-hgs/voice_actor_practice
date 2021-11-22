25023000c3f220a5ec240ac31221bab7824f5867
IZUMI HIGASHIYAMA <higashi10227193@gmail.com>
2021-11-22 17:01:19 +0900

    add:ルーティングquote/create追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :quotes, only: %i[index show edit update new]
+  resources :quotes, only: %i[index show edit update new create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'static_pages#top'
 end
