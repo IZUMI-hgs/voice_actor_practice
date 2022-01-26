6475693953b720b518bb38f1555162ba38fb5db1
IZUMI HIGASHIYAMA <higashi10227193@gmail.com>
2022-01-26 23:24:26 +0900

    fix:エラーメッセージ修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  rescue_from ActiveRecord::RecordNotFound, with: :render_404
-  rescue_from ActionController::RoutingError, with: :render_404
-  rescue_from Exception, with: :render_500
+  rescue_from Exception, with: :render500
+  rescue_from ActiveRecord::RecordNotFound, with: :render404
 
-  def render_404(exception = nil)
-    if exception
-      logger.info "Rendering 404 with exception: #{exception.message}"
-    end
-    render template: "errors/error_404", status: 404, layout: 'application'
+  def render404(exception = nil)
+    render template: 'errors/error404', status: 404, layout: 'application'
   end
 
-  def render_500(exception = nil)
-    if exception
-      logger.info "Rendering 500 with exception: #{exception.message}"
-    end
-    render template: "errors/error_500", status: 500, layout: 'application'
+  def render500(exception = nil)
+    logger.error(error.message)
+    logger.error(error.backtrace.join("\n"))
+    render template: 'errors/error500', status: 500, layout: 'application'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'static_pages#top'
-  get '*path', to: 'application#render_404'
+  get '*path', to: 'application#render404'
 end
