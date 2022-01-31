266aef825e8ad3c2f7bd71b0469f2d8207dc26a6
IZUMI HIGASHIYAMA <higashi10227193@gmail.com>
2022-01-31 13:02:21 +0900

    fix:エラーページ修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,16 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  #if Rails.env.production?
+    rescue_from Exception, with: :render500
+    rescue_from ActiveRecord::RecordNotFound, with: :render404
+  #end
 
-  rescue_from Exception, with: :render500
-  rescue_from ActiveRecord::RecordNotFound, with: :render404
-
-  def render404(exception = nil)
-    render template: 'errors/error404', status: 404, layout: 'application'
+  def render404
+    render file: "/app/views/errors/error404.html", status: 404
   end
 
-  def render500(exception = nil)
+  def render500(error)
     logger.error(error.message)
     logger.error(error.backtrace.join("\n"))
-    render template: 'errors/error500', status: 500, layout: 'application'
+    render file: "/app/views/errors/error500.html", status: 500
   end
 end
